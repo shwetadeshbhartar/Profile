f70c624e63941f57c01ca53d3cbac441eaba95af
Shweta Deshbhartar <shwetadeshbhartar@gmail.com>
2020-05-22 09:26:03 +0530

    My Profile

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Portfolio</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="assets/css/style.css">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+<!--navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">
+    <img src="assets/images/logo.png" alt="" width="140" height="50">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#about">About Me</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#project">Projects</a>
+      </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="#gallery">Gallery</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#contact">Contact Me</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/2.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="color: #000">Shweta Deshbhartar</h3>
+        <p style="color: #000">Yesterday is history, Tomorrow is a mystery<br>but...<br>Today is Gift, That's why it is called "Present".</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/11.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="color: #000">Shweta Deshbhartar</h3>
+        <p style="color: #000">Yesterday is history, Tomorrow is a mystery<br>but...<br>Today is Gift, That's why it is called "Present".</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/5.png" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3 style="color: #000">Shweta Deshbhartar</h3>
+        <p style="color: #000">Yesterday is history, Tomorrow is a mystery<br>but...<br>Today is Gift, That's why it is called "Present".</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="My-5">
+	<div class="py-5">
+		<h1 class="text-center" id="about">About Me</h1>
+	</div>
+	<div class="container">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="assets/images/sweetu.jpg" class="img-fluid aboutimg">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2>I am Shweta Deshbhartar</h2>
+			<p class="py-3">I'm <strong>Shweta Deshbhartar</strong>, i'm from seoni. I am currently pursuing in information technology 
+        from SGSITS college. I did my intermediate from state board with 73.66% and high school from state board 
+        with 73.6%. <br>My <strong>hobbies</strong> are listing song, playing badminton, watching korean dramas and also cricket.
+        My strength is takes initiative to work independently.<br> My <strong>weakness</strong> is i feel uncomfortable, 
+        until i finished my work in the given time. <br>My <strong>short term goal</strong> is to get a job in reputed company. 
+       <br> my <strong>long term goal</strong> is to become responsible & knowledgeable, personalilty & on respected position in a company</p>
+				<a href="#contact" class="btn btn-success"> Cheak more</a>
+	</div> 
+	</div>
+</section>
+<!--Resume-->
+<div class="container">
+    <div class="row">
+      <div class="col call-to-action section-description wow fadeInLeftBig">
+      <br>
+      <h3 style="text-align: center;">
+        Here is my resume you can download.
+      </h3>
+      <br>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col section-bottom-button wow fadeInUp" style="text-align: center;">
+        <a href="https://drive.google.com/open?id=11up_47HgKsbPrdvz-ArgkxKHovumENM8" alt="">
+       <button class="btn btn-primary btn-link-3 scroll-link" type="submit">Download</button>
+      </a>
+    </div>
+    </div>
+  </div>
+
+<!--Projects-->
+
+<section class="My-5">
+	<div class="py-5">
+		<h1 class="text-center" id="project">Projects</h1>
+	</div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/proassist.jpg" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">ProAssist-A Project Management System</h4>
+        <p class="card-text">The purpose of this wed based project is to build a portal that holds information about college 
+        students working on various projects in different domains and allow a user to find mentor and study
+         references for their project. (Only Documentation).</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+      </div>
+      
+       <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/books.jpg" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">Online Book Store System</h4>
+        <p class="card-text">The purpose of the project is to make full functional online book store system that allow its users 
+          to search and purchase a book online. The selected books will be displayed in a tabular form and then
+           user can pick and order one or more books online.<br>
+          Progamming languages: HTML, CSS, Javascript, Servlet, CGI, MySQL.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div> 
+      </div>
+      
+       <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/tic-tac-toe.png" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">Tic-Tac-Toe</h4>
+        <p class="card-text">A game in which two players alternately put X’s and O’s in compartments of a figure formed by
+           two vertical lines crossing two horizontal lines and each tries to get a row of three X’s or three 
+           O’s before the opponent does.<br>
+          Programming languages: HTML, CSS, Javascript.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+      </div>
+
+             <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/android.png" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">Directory Management Application</h4>
+        <p class="card-text">Developed an android application for the management of official account of student, faculties,
+         professionals etc and establish connection among them<br>
+        User need to register and provide the basic details to create the account which they can edit
+         update to maintain their professional portfolio among with their academic history and interests.<br>
+        IDE used: Android Studio<br>
+        Programming language: XML, Java, /MYSQL.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+      </div>
+
+             <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/python.png" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">Data Science using Python (Training Program)</h4>
+        <p class="card-text">Understanding of Pakages Numpy, Pandas, Scipy, Matlplot.<br>
+        Web – Scrapping of Ecommerce.<br>
+        Social Media Analytics.<br>
+        Statistical Anaysis.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="margin-bottom: 20px;">
+      <img class="card-img-top" src="assets/images/calculator.png" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">Calculator</h4>
+        <p class="card-text">A calculator is a device that performs arithmetic operations on numbers. 
+        The simplest calculators can do only addition, subtraction, multiplication, and division  and more.<br>
+        Programming languages: HTML, CSS.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+      </div> 
+
+    </div>
+  </div>
+	</section>
+
+<!--gallary-->
+
+<section class="My-5">
+  <div class="py-5">
+    <h1 class="text-center" id="gallery">Gallery</h1>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="assets/images/shruti.jpg" class="img-fluid pb-4">
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="assets/images/friend1.jpg" class="img-fluid pb-4">
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="assets/images/friend2.jpg" class="img-fluid pb-4">
+      </div>
+
+    </div>
+    
+  </div>
+  </section>
+
+<!--Contact-->
+
+
+<section class="My-5">
+  <div class="py-5">
+    <h1 class="text-center" id="contact">Contact Me</h1>
+  </div>
+  <div id="error_message"></div>
+  <div class="container">
+  <div class="w-100 m-auto">
+    <form action="userinfo.php" method="post" id="myform" onsubmit="return validate();">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" id="user" autocomplete="off" placeholder="Enter Your Username" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Email Id</label>
+        <input type="text" name="email" id="email" autocomplete="off" placeholder="Enter Your Email Id" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Mobile</label>
+        <input type="text" name="mobile" id="mobile" autocomplete="off" placeholder="Enter Your Mobile Number" class="form-control">
+      </div>
+       <div class="form-group">
+        <label>Comments</label>
+        <textarea class="form-control" id="comment" placeholder="Write Message Here...."></textarea>
+      </div>
+      <button class="btn btn-success" type="Submit" name="comments">Submit</button>
+    </form>
+  </div>
+  </div>
+  <script>
+        function validate(){
+          var user = document.getElementById("user").value;
+          var email = document.getElementById("email").value;
+          var mobile = document.getElementById("mobile").value;
+          var comment = document.getElementById("comment").value;
+          var error_message = document.getElementById("error_message");
+          
+          error_message.style.padding = "10px";
+          
+          var text;
+          if(user.length < 5){
+            text = "Please Enter Valid Username";
+            error_message.innerHTML = text;
+            return false;
+          }
+          if(email.indexOf("@") == -1 || email.length < 6){
+            text = "Please Enter Valid Email";
+            error_message.innerHTML = text;
+            return false;
+          }
+          if(isNaN(mobile) || mobile.length != 10){
+            text = "Please Enter Valid Mobile Number";
+            error_message.innerHTML = text;
+            return false;
+          }
+          if(comment.length <= 50){
+            text = "Please Enter More Than 20 Characters";
+            error_message.innerHTML = text;
+            return false;
+          }
+          alert("Form Submitted Successfully!");
+          return true;
+        }
+        </script>
+</section>
+
+<!--footer-->
+<section>
+  <div style="background-color: #424242; margin-top: 20px;">
+  <h6 style="text-align: center;color: #fff;">Other Sources:-</h6>
+  <div id="lab_social_icon_footer" >
+    <div class="text-center center-block">
+      <a href="https://www.facebook.com/shweta.deshbhartar?ref=bookmarks"><i id="social-fb" class="fa fa-facebook-square fa-3x social" ></i></a>
+      <a href="https://twitter.com/Shweta43722984"><i id="social-tw" class="fa fa-twitter-square fa-3x social" ></i></a>
+      <a href="https://www.linkedin.com/in/shweta-deshbhartar-06148117a/"><i id="social-gp" class="fa fa-linkedin-square fa-3x social"></i></a>
+      <p class=" text-white text-center">shwetadeshbhartar@gmail.com</p>
+      <p class=" text-white text-center">@shwetadeshbhartar20/05/20. All right reserved</p>
+    </div>
+    
+  </div>
+  </div>
+    
+</section>
+	<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
